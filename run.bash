a174615755b600f6d52a69024bf4dd650ad4599a
joergrs <joergschroeter84@gmail.com>
2022-11-19 23:07:23 +0100

    hint for venv initaliziation

--- run.bash
+++ run.bash
@@ -3,7 +3,13 @@
 SCRIPT_DIR=$(dirname "$0")
 cd $SCRIPT_DIR
 echo "Executing in `pwd`"
+
+# initial setup of virtual environment:
+# python3 -m venv venv
+
 source ./venv/bin/activate
+
+# pip install -r requirements.txt
 
 python __main__.py 2>&1
 
